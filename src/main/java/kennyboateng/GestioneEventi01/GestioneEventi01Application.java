87ac40adba1196992d3dee7b3ee583744eaa980d
Kenny Boateng <kennyboateng.99@gmail.com>
2024-09-20 13:00:59 +0200

    Initial commit

---
+++ src/main/java/kennyboateng/GestioneEventi01/GestioneEventi01Application.java
@@ -0,0 +1,13 @@
+package kennyboateng.GestioneEventi01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventi01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventi01Application.class, args);
+	}
+
+}
---
+++ src/test/java/kennyboateng/GestioneEventi01/GestioneEventi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kennyboateng.GestioneEventi01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventi01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
